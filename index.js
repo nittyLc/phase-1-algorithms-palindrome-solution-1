032568fb8b462ed0a179da3d6884f7e2e8e7daf0
caleb <musyokacaleb5@gmail.com>
2024-12-23 10:05:09 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
+// function isPalindrome(word) {
+//   // Write your algorithm here
+// }
+
+// /* 
+//   Add your pseudocode here
+// */
+
+// /*
+//   Add written explanation of your solution here
+// */
+
+// // You can run `node index.js` to view these console logs
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
+
+//   console.log("");
+
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+// }
+
+// module.exports = isPalindrome;
+
 function isPalindrome(word) {
-  // Write your algorithm here
-}
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  // Reverse the word and compare it with the original
+  const reversedWord = word.split("").reverse().join("");
+  return word === reversedWord;
 }
 
 module.exports = isPalindrome;
